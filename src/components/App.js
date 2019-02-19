cb80212da2c6d97cc92960da6694b0cb3c4331f0
natekandler <natekandler@gmail.com>
2019-02-18 16:46:55 -0800

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Table from './Table'
+
+const App = () => {
+  return (
+    <div className="ui container">App
+      <Table />
+    </div>
+  )
+}
+
+export default App;
